908d2fc743563b9d4f6ce698186a7589cb235bf2
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-01-30 22:08:23 -0300

    feat: add initial header component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
+import Header from "../_components/header";
 
 export default function Home() {
   return (
-  <h1>Full Stack Week</h1>
+    <div><Header /></div>
   );
 }
