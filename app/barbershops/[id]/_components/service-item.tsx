fd96eb635e47fd90b1d54490305305ce0176751a
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-03-26 01:21:11 -0300

    style: add vertical scroll to booking

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -148,7 +148,7 @@ const ServiceItem = ({ barbershop, service, isAuthenticated }: ServiceItemProps)
                                 <SheetTrigger asChild>
                                     <Button onClick={handleBookingClick} variant="secondary">Reservar</Button>
                                 </SheetTrigger>
-                                <SheetContent className="p-0">
+                                <SheetContent className="p-0 overflow-y-scroll">
                                     <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
                                         <SheetTitle>Fazer Reserva</SheetTitle>
                                     </SheetHeader>
