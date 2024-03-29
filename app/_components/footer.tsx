cf27c133385bb9b30750cbb86ce4199f5ed148a3
Guilherme Fonseca <guilhermao1234@hotmail.com>
2024-03-28 22:44:08 -0300

    feat: ensure semantic tags are used on footer and header

--- app/_components/footer.tsx
+++ app/_components/footer.tsx
@@ -1,11 +1,12 @@
 const Footer = () => {
-    return ( 
-        <div className="w-full bg-secondary py-6 px-5">
-            <p className="text-gray-400 text-xs font-bold opacity-75">₢ 2024 Copyright FSW Barber</p>
-        </div>
-
-     );
+    return (
+        <footer>
+            <div className="w-full bg-secondary py-6 px-5">
+                <p className="text-gray-400 text-xs font-bold opacity-75">₢ 2024 Copyright FSW Barber</p>
+            </div>
+        </footer>
+    );
 }
- 
+
 export default Footer;
 <div className="w-full bg-secondary"></div>
--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -11,24 +11,25 @@ import Link from "next/link";
 const Header = () => {
 
     return (
-        <Card>
-            <CardContent className="p-5 justify-between items-center flex flex-row">
-                <Link href={"/"}>
-                <Image src="/logo.png" alt="FSW Barber" height={22} width={130} />
-                </Link>
-                <Sheet>
-                    <SheetTrigger>
-                        <Button variant="outline" size="icon" className="h-8 w-8">
-                            <MenuIcon size={16} />
-                        </Button>
-                    </SheetTrigger>
-                    <SheetContent className="p-0">
-                        <SideMenu />
-                    </SheetContent>
-                </Sheet>
-            </CardContent>
-        </Card>
-
+        <header>
+            <Card>
+                <CardContent className="p-5 justify-between items-center flex flex-row">
+                    <Link href={"/"}>
+                        <Image src="/logo.png" alt="FSW Barber" height={22} width={130} />
+                    </Link>
+                    <Sheet>
+                        <SheetTrigger>
+                            <Button variant="outline" size="icon" className="h-8 w-8">
+                                <MenuIcon size={16} />
+                            </Button>
+                        </SheetTrigger>
+                        <SheetContent className="p-0">
+                            <SideMenu />
+                        </SheetContent>
+                    </Sheet>
+                </CardContent>
+            </Card>
+        </header>
     );
 }
 
